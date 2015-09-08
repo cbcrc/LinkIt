08b461428918418b78cc96d7a1207620d15af9d6
Stephane Leblanc <stephane.leblanc@radio-canada.ca>
2015-09-08 15:48:55 -0400

    Validate that we support polymorphic reference (as opposed to polymorphic referenced object with non-polymorphic reference)

---
+++ HeterogeneousDataSources.Tests/Exploratory/NestedPolymorphicReferenceTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using ApprovalTests.Reporters;
+using HeterogeneousDataSources.LoadLinkExpressions;
+using HeterogeneousDataSources.LoadLinkExpressions.Polymorphic;
+using HeterogeneousDataSources.Tests.Shared;
+using NUnit.Framework;
+using RC.Testing;
+
+namespace HeterogeneousDataSources.Tests.Polymorphic {
+    [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
+    public class NestedPolymorphicReferenceTests {
+
+        private LoadLinkProtocolFactory<WithNestedPolymorphicReference, string> _loadLinkProtocolFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loadLinkProtocolFactory = new LoadLinkProtocolFactory<WithNestedPolymorphicReference, string>(
+                loadLinkExpressions: new List<ILoadLinkExpression> {
+                    new RootLoadLinkExpression<WithNestedPolymorphicReferenceLinkedSource, WithNestedPolymorphicReference, string>(),
+                    new PolymorphicNestedLinkedSourcesLoadLinkExpression<WithNestedPolymorphicReferenceLinkedSource, object, object, Type>(
+                        linkedSource => linkedSource.Model.PolyIds,
+                        linkedSource => linkedSource.Contents, 
+                        (linkedSource, childLinkedSource) => linkedSource.Contents = childLinkedSource,
+                        link => link.GetType(),
+                        new Dictionary<
+                            Type, 
+                            IPolymorphicNestedLinkedSourceInclude<object, object>
+                        >{
+                            {
+                                typeof(string), 
+                                new PolymorphicNestedLinkedSourceInclude<
+                                    object,
+                                    object,
+                                    PersonLinkedSource,
+                                    Person,
+                                    string
+                                >(
+                                    link => (string)link
+                                )
+                            },
+                            {
+                                typeof(int), 
+                                new PolymorphicNestedLinkedSourceInclude<
+                                    object,
+                                    object,
+                                    PolymorphicNestedLinkedSourcesTests.ImageWithContextualizationLinkedSource,
+                                    Image,
+                                    string
+                                >(
+                                    link => ((int)link).ToString()
+                                )
+                            },
+                        }
+                    ),
+                },
+                getReferenceIdFunc: reference => reference.Id
+            );
+        }
+
+        [Test]
+        public void LoadLink_NestedPolymorphicReference() {
+            var sut = _loadLinkProtocolFactory.Create(
+                new WithNestedPolymorphicReference {
+                    Id = "1",
+                    PolyIds = new List<object>{"p1",32}
+                }
+            );
+
+            var actual = sut.LoadLink<WithNestedPolymorphicReferenceLinkedSource>("1");
+
+            ApprovalsExt.VerifyPublicProperties(actual);
+        }
+
+        public class WithNestedPolymorphicReferenceLinkedSource : ILinkedSource<WithNestedPolymorphicReference> {
+            public WithNestedPolymorphicReference Model { get; set; }
+            public List<object> Contents { get; set; }
+        }
+
+        public class WithNestedPolymorphicReference {
+            public string Id { get; set; }
+            public List<object> PolyIds { get; set; }
+        }
+
+        
+    }
+}
