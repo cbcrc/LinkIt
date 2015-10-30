d2253eb1661babbeb698cf55c2fb631ff35c0cd0
Stephane Leblanc <stephane.leblanc@radio-canada.ca>
2015-10-30 15:17:12 -0400

    Simplify link target factory

--- HeterogeneousDataSources/LinkTargets/LinkTargetFactory.cs
+++ HeterogeneousDataSources/LinkTargets/LinkTargetFactory.cs
@@ -5,16 +5,14 @@ using System.Reflection;
 
 namespace HeterogeneousDataSources
 {
-    //Adapted from:
+    //Simplification of the solution shown at:
     //http://stackoverflow.com/questions/7723744/expressionfunctmodel-string-to-expressionactiontmodel-getter-to-sette
     public static class LinkTargetFactory {
 
+        #region Single Value
         public static LinkTargetBase<TLinkedSource, TTargetProperty> Create<TLinkedSource, TTargetProperty>(
-            Expression<Func<TLinkedSource, TTargetProperty>> linkTargetGetterFunc) 
-        {
+            Expression<Func<TLinkedSource, TTargetProperty>> linkTargetGetterFunc) {
             PropertyInfo property = GetPropertyFromGetter(linkTargetGetterFunc);
-            EnsureNoWriteOnlyProperty(property);
-            EnsureNoReadOnlyProperty(property);
 
             return new SingleValueLinkTarget<TLinkedSource, TTargetProperty>(
                 property.Name,
@@ -22,12 +20,19 @@ namespace HeterogeneousDataSources
             );
         }
 
+        private static Action<TLinkedSource, TTargetProperty> CreateSingleValueLinkTargetSetterAction<TLinkedSource, TTargetProperty>(PropertyInfo property) {
+            MethodInfo setter = property.GetSetMethod();
+            return (Action<TLinkedSource, TTargetProperty>)Delegate.CreateDelegate(
+                typeof(Action<TLinkedSource, TTargetProperty>),
+                setter
+            );
+        } 
+        #endregion
+
+        #region Multi Value
         public static LinkTargetBase<TLinkedSource, TTargetProperty> Create<TLinkedSource, TTargetProperty>(
-            Expression<Func<TLinkedSource, List<TTargetProperty>>> linkTargetGetterFunc) 
-        {
+            Expression<Func<TLinkedSource, List<TTargetProperty>>> linkTargetGetterFunc) {
             PropertyInfo property = GetPropertyFromGetter(linkTargetGetterFunc);
-            EnsureNoWriteOnlyProperty(property);
-            EnsureNoReadOnlyProperty(property);
 
             return new MultiValueLinkTarget<TLinkedSource, TTargetProperty>(
                 property.Name,
@@ -36,74 +41,65 @@ namespace HeterogeneousDataSources
             );
         }
 
-        private static Action<TLinkedSource, TTargetProperty> CreateSingleValueLinkTargetSetterAction<TLinkedSource, TTargetProperty>(PropertyInfo property)
-        {
-            MethodInfo setter = property.GetSetMethod();
-            return (Action<TLinkedSource, TTargetProperty>) Delegate.CreateDelegate(
-                typeof (Action<TLinkedSource, TTargetProperty>),
-                setter
-            );
-        }
-
-        //stle: move to type constructor?
-        //stle: http://stackoverflow.com/questions/7723744
         private static Action<TLinkedSource, List<TTargetProperty>> CreateMultiValueLinkTargetSetterAction<TLinkedSource, TTargetProperty>(PropertyInfo property) {
             MethodInfo setter = property.GetSetMethod();
             return (Action<TLinkedSource, List<TTargetProperty>>)Delegate.CreateDelegate(
                 typeof(Action<TLinkedSource, List<TTargetProperty>>),
                 setter
             );
-        }
+        } 
+        #endregion
 
-
+        #region Shared
         private static PropertyInfo GetPropertyFromGetter<TLinkedSource, TTargetProperty>(Expression<Func<TLinkedSource, TTargetProperty>> getter) {
+
             EnsureNoFunctionOrAction(getter);
 
             var getterBody = (MemberExpression)getter.Body;
             EnsureNoNestedProperty(getterBody);
             EnsureNoMemberAccess(getterBody);
 
-            return (PropertyInfo) getterBody.Member;
+            var property = (PropertyInfo)getterBody.Member;
+
+            EnsureNoWriteOnlyProperty(property);
+            EnsureNoReadOnlyProperty(property);
+            return property;
         }
 
-        private static void EnsureNoReadOnlyProperty(PropertyInfo property)
-        {
-            if (property.CanRead == false){
+        private static void EnsureNoReadOnlyProperty(PropertyInfo property) {
+            if (property.CanRead == false) {
                 throw new ArgumentException("Only read-write property are supported");
             }
         }
 
-        private static void EnsureNoWriteOnlyProperty(PropertyInfo property)
-        {
-            if (property.CanWrite == false){
+        private static void EnsureNoWriteOnlyProperty(PropertyInfo property) {
+            if (property.CanWrite == false) {
                 throw new ArgumentException("Only read-write property are supported");
             }
         }
 
-        private static void EnsureNoMemberAccess(MemberExpression getterBody)
-        {
-            if (getterBody.Member.MemberType != MemberTypes.Property){
+        private static void EnsureNoMemberAccess(MemberExpression getterBody) {
+            if (getterBody.Member.MemberType != MemberTypes.Property) {
                 throw OnlyDirectGetterAreSupported();
             }
         }
 
-        private static void EnsureNoNestedProperty(MemberExpression getterBody)
-        {
+        private static void EnsureNoNestedProperty(MemberExpression getterBody) {
             var getterBodyExpression = getterBody.Expression;
-            if (getterBodyExpression.NodeType != ExpressionType.Parameter){
+            if (getterBodyExpression.NodeType != ExpressionType.Parameter) {
                 throw OnlyDirectGetterAreSupported();
             }
         }
 
-        private static void EnsureNoFunctionOrAction<TLinkedSource, TTargetProperty>(Expression<Func<TLinkedSource, TTargetProperty>> getter)
-        {
-            if (getter.Body.NodeType != ExpressionType.MemberAccess){
+        private static void EnsureNoFunctionOrAction<TLinkedSource, TTargetProperty>(Expression<Func<TLinkedSource, TTargetProperty>> getter) {
+            if (getter.Body.NodeType != ExpressionType.MemberAccess) {
                 throw OnlyDirectGetterAreSupported();
             }
         }
 
         private static ArgumentException OnlyDirectGetterAreSupported() {
             return new ArgumentException("Only direct getter are supported. Ex: p => p.Property");
-        }
+        } 
+        #endregion
     }
 }
