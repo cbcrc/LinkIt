ebd020b76ee4cc2659940589f0323eba9949cd66
Stephane Leblanc <stephane.leblanc@radio-canada.ca>
2015-11-05 14:54:56 -0500

    Add files

---
+++ HeterogeneousDataSource.Conventions/DefaultConventions/LoadLinkReferenceWhenLinkedSourceModelPropertyHasIdSuffixConvention.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using HeterogeneousDataSources;
+
+namespace HeterogeneousDataSource.Conventions.DefaultConventions {
+    public class LoadLinkReferenceWhenLinkedSourceModelPropertyHasIdSuffixConvention : ILoadLinkExpressionConvention {
+        public bool DoesApply(
+            PropertyInfo linkTargetProperty,
+            PropertyInfo linkedSourceModelProperty) 
+        {
+            return
+                linkTargetProperty.GetLinkTargetKind() == LinkTargetKind.SingleValue &&
+                linkTargetProperty.MatchLinkedSourceModelPropertyName(linkedSourceModelProperty, "Id");
+        }
+
+        public void Apply<TLinkedSource, TLinkTargetProperty, TLinkedSourceModelProperty>(
+            LoadLinkProtocolForLinkedSourceBuilder<TLinkedSource> loadLinkProtocolForLinkedSourceBuilder, 
+            Expression<Func<TLinkedSource, TLinkTargetProperty>> getLinkTargetProperty,
+            Func<TLinkedSource, TLinkedSourceModelProperty> getLinkedSourceModelProperty, 
+            PropertyInfo linkTargetProperty, 
+            PropertyInfo linkedSourceModelProperty)
+        {
+            loadLinkProtocolForLinkedSourceBuilder.LoadLinkReference(
+                getLinkedSourceModelProperty,
+                getLinkTargetProperty
+            );
+        }
+    }
+}
