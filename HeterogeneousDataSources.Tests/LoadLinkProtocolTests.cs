89ac2ac65b4cd2fd761b3e298b166ef1966ef977
Stephane Leblanc <stephane.leblanc@radio-canada.ca>
2015-11-09 16:54:23 -0500

    LoadLink_LinkedSourceWithoutLoadLinkExpressionAtRoot_ShouldThrow

--- HeterogeneousDataSources.Tests/LoadLinkProtocolTests.cs
+++ HeterogeneousDataSources.Tests/LoadLinkProtocolTests.cs
@@ -25,5 +25,21 @@ namespace HeterogeneousDataSources.Tests
 
             Assert.That(fakeReferenceLoader.IsDisposed, Is.True);
         }
+
+        [Test]
+        public void LoadLink_LinkedSourceWithoutLoadLinkExpressionAtRoot_ShouldThrow() {
+            var loadLinkProtocolBuilder = new LoadLinkProtocolBuilder();
+            var fakeReferenceLoader = new FakeReferenceLoader<Person, string>(reference => reference.Id);
+            var sut = loadLinkProtocolBuilder.Build(fakeReferenceLoader);
+
+            TestDelegate act = () => sut.LoadLink<PersonLinkedSource>().ById("dont-care");
+
+            Assert.That(act, Throws.InvalidOperationException
+                .With.Message.Contains("PersonLinkedSource").And
+                .With.Message.Contains("root linked source")
+            );
+
+        }
+
     }
 }
